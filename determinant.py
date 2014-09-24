55512728f1818714e8f8232aa2c5b7b7fc6473b1
luntc@bc.edu <cameronlunt@camerons-air.bc.edu>
2014-09-23 23:20:14 -0400

    variable name changes

--- determinant.py
+++ determinant.py
@@ -1,12 +1,13 @@
 import copy
 
-def d(n):
+
+def det(matrix):
     determinant = 0
-    for i in n:
-        if len(i) != len(n):
-            return False
-    if len(n) == 2:
-        return n[0][0]*n[1][1] - n[0][1]*n[1][0]
+    for row in matrix:
+        if len(row) != len(matrix):
+            raise ValueError('Not a square matrix')
+    if len(matrix) == 2:
+        return matrix[0][0]*matrix[1][1] - matrix[0][1]*matrix[1][0]
     else:
         for i in range(len(n[0])):
             sub_determinant = d(sub_matrix(i,n))
@@ -20,4 +21,5 @@ def sub_matrix(index, matrix):
     for row in new_matrix:
         row.pop(index)
     return new_matrix
+
 
